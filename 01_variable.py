b3c0b142743f73d2b1e11a19fd693db88ecaa13c
choseongchan3712 <choseongchan3712@naver.com>
2025-07-08 20:06:10 +0900

    variable

---
+++ 01_variable.py
@@ -0,0 +1,15 @@
+print(4990)
+print(4990 * 2)
+print(4990 + 149)
+print(4990 * 3 + 1490 * 2 + 1250 * 5)
+
+print("-----------------")
+
+burger_price = 4750
+fries_price = 1490
+drink_price = 1250
+
+print(burger_price)
+print(burger_price * 2)
+print(burger_price + fries_price)
+print(burger_price * 3 + fries_price * 2 + drink_price * 5)
