6b2a9a53896ce9118c7c4be8f80c8a2e86094bbc
choseongchan3712 <choseongchan3712@naver.com>
2025-07-09 19:59:42 +0900

    boolean

---
+++ 09_boolean.py
@@ -0,0 +1,54 @@
+print(True)
+# True
+print(False)
+# False
+
+print(True and True)
+# True
+print(True and False)
+# False
+print(False and True)
+# False
+print(False and False)
+# False
+
+print("----------")
+
+print(True or True)
+# True
+print(True or False)
+# True
+print(False or True)
+# True
+print(False or False)
+# False
+
+print("----------")
+
+print(not True)
+# False
+print(not False)
+# True
+
+print("----------")
+
+print(2 < 1)
+# False
+print(2 > 1)
+# True
+print(3 >= 2)
+# True
+print(3 <= 2)
+# False
+print(2 == 2)
+# True
+print(2 != 2)
+# False
+
+print("----------")
+
+print("Hello" == "Hello")
+# True
+print("Hello" != "Hello")
+# False
+
