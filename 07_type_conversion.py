122bbfd16382124b563b4439b1054b619b8d0c39
choseongchan3712 <choseongchan3712@naver.com>
2025-07-09 19:15:19 +0900

    type_conversion

---
+++ 07_type_conversion.py
@@ -0,0 +1,23 @@
+print(int(3.8))
+# 3
+
+print(float(3))
+# 3.0
+
+print(int("2") + int("5"))
+# 7
+
+print(float("1.1") + float("2.5"))
+# 3.6
+
+print(str(2) + str(5))
+# 25
+
+print("--------------")
+
+age = 7
+print("제 나이는 " + str(age) + "살입니다.")
+# 제 나이는 7살입니다.
+
+print(int("Hello world!"))
+# error
