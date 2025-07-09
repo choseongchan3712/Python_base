f72be35ede381808ccb880bdafa65a648896c70e
choseongchan3712 <choseongchan3712@naver.com>
2025-07-09 19:05:04 +0900

    string

---
+++ 06_string.py
@@ -0,0 +1,18 @@
+print("Hello")
+# Hello
+print("World")
+# World
+
+print("i'm excited to learn Python")
+# i'm excited to learn Python
+print("i\'m \"excited\" to learn Python")
+# i'm "excited" to learn Python
+
+print('Hello' + 'World')
+# HelloWorld
+
+print('Hello' * 3)
+# HelloHelloHello
+
+print("3" + "5")
+# 35
