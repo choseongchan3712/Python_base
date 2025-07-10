3d9389f70dd338e7011e4dc7664d37d01e3ee21e
choseongchan3712 <choseongchan3712@naver.com>
2025-07-10 19:57:12 +0900

    constant

---
+++ 12_constant.py
@@ -0,0 +1,12 @@
+# 상수
+PI = 3.14
+
+def calculate_area(r):
+    return PI * r * r
+
+radius = 4
+print(f"반지름이 {radius}면, 넓이는 {calculate_area(radius): .2f}")
+radius = 6
+print(f"반지름이 {radius}면, 넓이는 {calculate_area(radius): .2f}")
+radius = 7
+print(f"반지름이 {radius}면, 넓이는 {calculate_area(radius): .2f}")
