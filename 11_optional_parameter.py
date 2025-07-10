ecba50a157bdfc663f5cc067f2f8bb1587d58b5c
choseongchan3712 <choseongchan3712@naver.com>
2025-07-10 19:36:13 +0900

    optional_parameter

---
+++ 11_optional_parameter.py
@@ -0,0 +1,16 @@
+def myself(name, age, nationality="한국"):
+    print(f"내 이름은 {name}")
+    print(f"나이는 {age}살")
+    print(f"국적은 {nationality}")
+
+myself("수달", 27, "미국")
+print("--------------")
+myself("수달", 27)
+
+# 내 이름은 수달
+# 나이는 27살
+# 국적은 미국
+# --------------
+# 내 이름은 수달
+# 나이는 27살
+# 국적은 한국
