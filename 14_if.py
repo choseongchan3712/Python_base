e3720176da2b39ad7218750bd582902fe5976564
choseongchan3712 <choseongchan3712@naver.com>
2025-07-11 19:22:14 +0900

    if

---
+++ 14_if.py
@@ -0,0 +1,23 @@
+# if & else
+
+temperture = 16
+
+if temperture <= 10:
+    print("자켓을 입느다.")
+else:
+    print("자켓을 입지 않는다.")
+
+# elif
+
+score = 75
+
+if score >= 90:
+    print("A")
+elif score >=80:
+    print("B")
+elif score >=70:
+    print("C")
+elif score >=60:
+    print("D")
+else:
+    print("F")
