469c0dcc5447c43bed9b58eecf477fda781e65d9
choseongchan3712 <choseongchan3712@naver.com>
2025-07-15 23:26:44 +0900

    dictionary

--- 18_dictionary.py
+++ 18_dictionary.py
@@ -19,3 +19,45 @@ my_otter = {
 }
 print(my_otter['son_otter'])
 # 수달
+
+print("---------------")
+
+print(my_otter.values())
+# dict_values(['수마', '수빠', '수달'])
+
+print('수달' in my_otter.values())
+# True
+
+print('해달' in my_otter.values())
+# False
+
+for value in my_otter.values():
+    print(value)
+# 수마
+# 수빠
+# 수달
+
+print(my_otter.keys())
+# dict_keys(['mother_otter', 'father_otter', 'son_otter'])
+
+for key in my_otter.keys():
+    print(key)
+# mother_otter
+# father_otter
+# son_otter
+
+for key in my_otter.keys():
+    value = my_otter[key]
+    print(key, value)
+# mother_otter 수마
+# father_otter 수빠
+# son_otter 수달
+
+print(my_otter.items())
+# dict_items([('mother_otter', '수마'), ('father_otter', '수빠'), ('son_otter', '수달')])
+
+for key, value in my_otter.items():
+    print(key, value)
+# mother_otter 수마
+# father_otter 수빠
+# son_otter 수달
